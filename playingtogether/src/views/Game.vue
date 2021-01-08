3006dd2c1f646cd005137b25ea8e1602936280c0
Matteo NOTTARIS <nottaris.matteo@gmail.com>
2021-01-08 17:56:00 +0100

    css

--- playingtogether/src/views/Game.vue
+++ playingtogether/src/views/Game.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="Host">
     <h1>This is a Game page</h1>
+    <UGame/>
   </div>
 </template>
+
+
+<script>
+
+import UGame from '../components/UserGame.vue';
+
+export default {
+  components:{
+    UGame,
+  }
+}
+</script>
